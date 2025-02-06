ccd21c273b31058a013dab9d67b70a252812a73b
Ronak1137 <hdgthf322@gmail.com>
2025-02-06 13:31:09 +0530

    Update b.py

--- b.py
+++ b.py
@@ -1,7 +1,7 @@
 import gdown
 
 # URL file mediafire
-url = "https://www.mediafire.com/file/do27y6t7m2ml0yz/WINDOWS7HOMEBASIC64-EN.zip/file?dkey=wst1rawlbbz&r=951"
+url = "https://download943.mediafire.com/w0tkbcxmy2pg08PGmCN6PSK6vchFwvJloXKbfIaQ2tgXseZ-5aw-wWI1pITNpFqI9dKnrz8mRtOz_tEqBATmRWi1-lwNBpffytrGdkadqIya9BmFBkrqs-yvtc0yStcrT2qgCcqHZu2ydeMs1JT8jfnJW_-75xhXRA9aTYjns930yWE/do27y6t7m2ml0yz/WINDOWS7HOMEBASIC64-EN.zip"
 
 # Đường dẫn lưu file sau khi tải
 output = "/mnt/win10.zip"  # Đổi tên file tùy ý
