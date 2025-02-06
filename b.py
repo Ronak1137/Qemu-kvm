84069e00ae29f7f15eac50bba43ce0ce389ae597
Ronak1137 <hdgthf322@gmail.com>
2025-02-06 13:59:43 +0530

    Update b.py

--- b.py
+++ b.py
@@ -4,7 +4,7 @@ import gdown
 url = "https://download943.mediafire.com/w0tkbcxmy2pg08PGmCN6PSK6vchFwvJloXKbfIaQ2tgXseZ-5aw-wWI1pITNpFqI9dKnrz8mRtOz_tEqBATmRWi1-lwNBpffytrGdkadqIya9BmFBkrqs-yvtc0yStcrT2qgCcqHZu2ydeMs1JT8jfnJW_-75xhXRA9aTYjns930yWE/do27y6t7m2ml0yz/WINDOWS7HOMEBASIC64-EN.zip"
 
 # Đường dẫn lưu file sau khi tải
-output = "/mnt/win10.zip"  # Đổi tên file tùy ý
+output = "/mnt/WINDOWS7HOMEBASIC64-EN.zip"  # Đổi tên file tùy ý
 
 # Tải file
 gdown.download(url, output, quiet=False)
