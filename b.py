c8277d4505b95ade16391a653e7e2ee59e83d93d
Ronak1137 <hdgthf322@gmail.com>
2025-02-07 14:48:11 +0530

    Update b.py

--- b.py
+++ b.py
@@ -1,6 +1,6 @@
 import gdown
 
-# URL file google drive
+# URL file media fire
 url = "https://download943.mediafire.com/3tde0tapzfcgmAHEBbtD-ooY9FbowyZgurwpQ9nuORnL7b7bgOBc5vhG-zD9mQ1C9cQNcjIgkYV5mAMAu_ym2A47c5A-wzM85KiowfR5V_FCplxyZ-CKP6tTUdPP7520DcdXvZ5DJlGuAQE80IwF_83piowZuJpu5nPtHrTchZVPn68/f4x3oz2w0aphghq/WINDOWS7HOMwzM85KiowfR5V_FCplxyZ-CKP6tTUdPP7520DcdXvZ5DJlGuAQE80IwF_83piowZuJpu5nPtHrTchZVPn68/f4x3oz2w0aphghq/WINDOWS7HOMEBASIC6w0aphghq/WINDOWS7HOMwzM85KiowfR5V_FCplxyZ-CKP6tTUdPP7520DcdXvZ5DJlGuAQE80IwF_83piowZuJpu5nPtHrTchZVPn68/f4x3oz2w0aphghq/WINDOWS7HOMEBASIC64-EN.7z"EBASIC64-EN.7z4-EN.7z"EBASIC64-EN.7z"
 
 # Đường dẫn lưu file sau khi tải
