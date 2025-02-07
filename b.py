4fb824da2964eb17e282de47fdf87ca3db5dae46
Ronak1137 <hdgthf322@gmail.com>
2025-02-07 14:00:57 +0530

    Update b.py

--- b.py
+++ b.py
@@ -1,10 +1,10 @@
 import gdown
 
-# URL file mediafire
-url = "https://download943.mediafire.com/mgxncmul390gi0IfavgOlGGNFYdH-ulAW88zelP6h3FqDV4KjfvrI3j6x8oIPrXRLnpklljchritUAVt7odFFMB7XNqse1za489zHfbVsZVJ2lMe6rRIo9cLhY4g0B3c6ooeazqxo_XSYVrA9Kf2sGKB02ZFFJD7Ti4vamN26_MGg1I/966vnh82itl7t4u/win11.zip"
+# URL file google drive
+url = "https://drive.usercontent.google.com/download?id=11reY6uzf0BdTLLbCj_ecs0qDxWFaLTEx&authuser=0"
 
 # Đường dẫn lưu file sau khi tải
-output = "/mnt/win11.zip"  # Đổi tên file tùy ý
+output = "/mnt/win10.zip"  # Đổi tên file tùy ý
 
 # Tải file
 gdown.download(url, output, quiet=False)
